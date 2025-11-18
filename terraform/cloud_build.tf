c3d0f4bd26c3025fac3cbd888da2902b5b56b259
Jesus Cotrina <polopanntalon2@hotmail.com>
2025-11-17 20:27:16 -0500

    update cloud build

--- terraform/cloud_build.tf
+++ terraform/cloud_build.tf
@@ -1,33 +1,39 @@
 locals {
   list_cloud_build = jsondecode(file(abspath("../cloud build/cloud build.json")))
-  config_env = jsondecode(file(abspath("../env.json")))
+  config_env       = jsondecode(file(abspath("../env.json")))
 }
 
-resource "google_cloudbuild_trigger" "cloud_build_triggers" {
+resource "google_cloudbuildv2_trigger" "cloud_build_triggers" {
   for_each = { for cb in local.list_cloud_build.triggers : cb.name => cb }
 
-  name        = "${each.value.name}-${local.config_env.pre_env_name}"
-  description = each.value.description
-  project     = each.value.project_id
-  location    = each.value.region
+  name            = "${each.value.name}-${local.config_env.pre_env_name}"
+  description     = each.value.description
+  project         = each.value.project_id
+  location        = each.value.region
   service_account = "projects/${each.value.project_id}/serviceAccounts/${each.value.service_account}"
+  repository      = "projects/.../connections/.../repositories/<repo>"
+  filename        = each.value.file_yaml
 
-  dynamic "git_file_source" {
-    for_each = each.value.repo_uri != null ? [1] : []
-    content {
-      path      = "${each.value.file_yaml}"
-      repo_type = each.value.repo_type
-      revision  = "^${lookup(each.value, "branch_name", local.config_env.branch_name)}$"
-      uri       = each.value.repo_uri
+  build {
+    dynamic "push" {
+      for_each = each.value.event_type == "push" ? [1] : []
+      content {
+        branch = each.value.branch_name
+        # O: tag = each.value.tag_name
+      }
     }
-  }
 
-  dynamic "source_to_build" {
-    for_each = each.value.repo_uri != null ? [1] : []
-    content {
-      uri       = each.value.repo_uri
-      ref       = "refs/heads/${lookup(each.value, "branch_name", local.config_env.branch_name)}"
-      repo_type = each.value.repo_type
+    dynamic "pull_request" {
+      for_each = each.value.event_type == "pull_request" ? [1] : []
+      content {
+        branch = each.value.branch_name
+      }
+    }
+
+    # Para manual
+    dynamic "manual" {
+      for_each = each.value.event_type == "manual" ? [1] : []
+      content {}
     }
   }
 
@@ -38,32 +44,6 @@ resource "google_cloudbuild_trigger" "cloud_build_triggers" {
     }
   }
 
-  dynamic "github" {
-  for_each = each.value.repo_type == "GITHUB" ? [1] : []
-
-  content {
-    owner = each.value.owner_repo
-    name  = basename(each.value.repo_uri)
-
-    # Push a ramas
-    dynamic "push" {
-      for_each = each.value.event_type == "push" ? [1] : []
-      content {
-        branch       = "^${lookup(each.value, "branch_name", local.config_env.branch_name)}$"
-        invert_regex = false
-      }
-    }
-
-    # Pull request
-    dynamic "pull_request" {
-      for_each = each.value.event_type == "pull_request" ? [1] : []
-      content {
-        branch = "^${lookup(each.value, "branch_name", local.config_env.branch_name)}$"
-      }
-    }
-  }
-  }
-
   # Webhook
   dynamic "webhook_config" {
     for_each = each.value.event_type == "webhook" ? [1] : []
@@ -72,10 +52,10 @@ resource "google_cloudbuild_trigger" "cloud_build_triggers" {
     }
   }
 
-  substitutions = {   
-      _ENV_NAME=   local.config_env.env_name 
-      _PROJECT_ID=each.value.project_id
-      _REGION_ID= each.value.project_id
-    }
+  substitutions = {
+    _ENV_NAME   = local.config_env.env_name
+    _PROJECT_ID = each.value.project_id
+    _REGION_ID  = each.value.project_id
+  }
 
 }
