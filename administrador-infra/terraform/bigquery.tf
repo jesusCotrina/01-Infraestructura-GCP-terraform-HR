c8f01727a198877296091c6f222a284e61410ea3
Jesus  Cotrina Narciso <jesus.cotrina@rimac.com.pe>
2025-11-18 11:42:49 -0500

    add datasets bigquery

---
+++ administrador-infra/terraform/bigquery.tf
@@ -0,0 +1,14 @@
+locals {
+  dataset_config = jsondecode(file("../bigquery/datasets.json"))
+}
+
+resource "google_bigquery_dataset" "dataset" {
+  for_each = {
+    for d in local.dataset_config : d.dataset_name => d
+  }
+
+  dataset_id    = each.value.dataset_name
+  location      = each.value.location
+  friendly_name = each.value.friendly_name
+  description   = each.value.description
+}
