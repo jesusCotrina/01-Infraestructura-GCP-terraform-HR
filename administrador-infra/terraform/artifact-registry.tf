a5bfe753132b0391c9e5f22322886728691e59ce
Jesus  Cotrina Narciso <jesus.cotrina@rimac.com.pe>
2025-11-19 15:50:39 -0500

    add artifact registry

---
+++ administrador-infra/terraform/artifact-registry.tf
@@ -0,0 +1,20 @@
+locals {
+  repos = jsondecode(file("../bigquery/artifact-registry.json")).repositories
+}
+
+resource "google_artifact_registry_repository" "repos" {
+  for_each = {for r in local.repos : r.id => r}
+  project       = each.value.project_id
+  location      = each.value.location
+  repository_id = each.value.id
+
+  description = lookup(each.value, "description", null)
+  format      = each.value.format
+
+  dynamic "docker_config" {
+    for_each = each.value.format == "DOCKER" ? [1] : []
+    content {
+      immutable_tags = false
+    }
+  }
+}
