e3e0c62fc6d1f0e0475f1a3b1e3f278335b46275
Jesus  Cotrina Narciso <jesus.cotrina@rimac.com.pe>
2025-11-23 01:23:53 -0500

    .

--- motor-busqueda-backend-infra/terraform/cloud_run.tf
+++ motor-busqueda-backend-infra/terraform/cloud_run.tf
@@ -5,49 +5,56 @@ locals {
 }
 
 
-resource "google_cloud_run_v2_service" "cloudrun_service" {
+resource "google_cloud_run_service" "cloudrun_service" {
   name     = local.config.service_name
   project  = local.env_vars.project
   location = local.config.location
 
   template {
-    containers {
-      image = "${local.env_vars.region}-docker.pkg.dev/${local.env_vars.project}/${local.config.repositorio}/backend-motor-busqueda:latest"
-
-      resources {
-        cpu_idle         = true
-        limits = {
-          cpu    = local.config.cpu
-          memory = local.config.memory
+    spec {
+      containers {
+        image = "${local.env_vars.region}-docker.pkg.dev/${local.env_vars.project}/${local.config.repositorio}/backend-motor-busqueda:latest"
+        resources {
+          limits = {
+            memory = local.config.memory
+            cpu    = tostring(
+                        tonumber(replace(local.config.memory, "Gi", "")) > 8 ? 4 : 
+                        tonumber(replace(local.config.memory, "Gi", "")) > 4 ? 2 : 1)
+          }
+        }
+        dynamic "env" {
+          for_each = each.value.env
+          content {
+            name  = env.key
+            value = env.value
+          }
         }
       }
-
-      # variables de entorno del JSON
-      dynamic "env" {
-        for_each = local.config.env
-        content {
-          name  = env.key
-          value = env.value
-        }
-      }
+      service_account_name        = local.env_vars.service_account_ejecucion
+      container_concurrency       = 100
+      timeout_seconds             = 3600
     }
 
-    scaling {
-      min_instance_count = 0
-      max_instance_count = 10
+    metadata {
+      annotations = {
+        "run.googleapis.com/client-name"  = "terraform"
+        "deployment-timestamp" = timestamp()   
+        "run.googleapis.com/ingress"      = "all"
+      }      
     }
   }
 
   traffic {
-    type  = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
-    percent = 100
+    percent         = 100
+    latest_revision = true
   }
+  
 }
 
-resource "google_cloud_run_v2_service_iam_member" "invoker" {
-  name     = google_cloud_run_v2_service.cloudrun_service.name
+resource "google_cloud_run_service_iam_member" "invoker" {
+  project  = local.env_vars.project
   location = local.config.location
+  service  = google_cloud_run_service.cloudrun_service.name
   role     = "roles/run.invoker"
   member   = "allUsers"
 }
-
